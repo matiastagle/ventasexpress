22290078aa36bdacdb9d1f257e8be8821d7f1fbf
cetecom <cetecom@LCV10415>
2019-03-29 16:02:30 -0300

    menu

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Ventas express</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+
+	<script src="js/jquery-3.2.1.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<header>
+		<span id="button-menu" class="fa fa-bars"></span>
+
+		<nav class="navegacion">
+			<ul class="menu">
+				<!-- TITULAR -->
+				<li class="title-menu">Todas las categorias</li>
+				<!-- TITULAR -->
+
+				<li><a href="#"><span class="fa fa-home icon-menu"></span>Inicio</a></li>
+
+				<li class="item-submenu" menu="1">
+					<a href="#"><span class="fa fa-suitcase icon-menu"></span>Ingresos</a>
+					<ul class="submenu">
+						<li class="title-menu"><span class="fa fa-suitcase icon-menu"></span>Ingresos</li>
+						<li class="go-back">Atras</li>
+
+						<li><a href="#">Productos</a></li>
+						<li><a href="#">Ventas</a></li>
+						<li><a href="#">Bodega</a></li>
+					</ul>
+				</li>
+
+				<li class="item-submenu" menu="2">
+					<a href="#"><span class="fa fa-shopping-bag icon-menu"></span>Informes</a>
+					<ul class="submenu">
+						<li class="title-menu"><span class="fa fa-shopping-bag icon-menu"></span>Informes</li>
+						<li class="go-back">Atras</li>
+
+						<li><a href="#">Ventas</a></li>
+		
+					</ul>
+				</li>
+
+				<li class="item-submenu" menu="3">
+					<a href="#"><span class="fa fa-shopping-bag icon-menu"></span>Consultas</a>
+					<ul class="submenu">
+						<li class="title-menu"><span class="fa fa-shopping-bag icon-menu"></span>Consultas</li>
+						<li class="go-back">Atras</li>
+
+						<li><a href="#">Productos</a></li>
+		
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
